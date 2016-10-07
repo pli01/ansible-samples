6e35f80a207c8f91ade488b917727773af67546d
pli01 <pli01@github.com>
2016-10-07 23:50:19 +0200

    unbuffered output

--- jobs/scripts/include-vars.sh
+++ jobs/scripts/include-vars.sh
@@ -4,5 +4,5 @@ cat > vars_file.yml <<EOF
 user: jenkins
 url: https://github.com/pli01/ansible-samples/archive/master.zip
 dest: ansible-samples-master
-post_cmd: ["( cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml )"]
+post_cmd: ["( export PYTHONUNBUFFERED=1 ; cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml )"]
 EOF
