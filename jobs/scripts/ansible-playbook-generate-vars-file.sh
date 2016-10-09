d56aebe5e0ca017bf50a44c36eac6db6d80ed8e6
pli01 <pli01@github.com>
2016-10-09 15:37:05 +0200

    myvar

--- jobs/scripts/ansible-playbook-generate-vars-file.sh
+++ jobs/scripts/ansible-playbook-generate-vars-file.sh
@@ -5,6 +5,6 @@ user: jenkins
 url: https://github.com/pli01/ansible-samples/archive/master.zip
 dest: ansible-samples-master
 pre_cmd: []
-post_cmd: ["( export PYTHONUNBUFFERED=1 ; cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml -vvv )"]
+post_cmd: ["( export PYTHONUNBUFFERED=1 ; cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml )"]
 EOF
 
