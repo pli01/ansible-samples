659c638bdb5289eb450b835a266aa6f777d7afb9
pli01 <pli01@github.com>
2016-10-09 16:58:10 +0200

    PYTHONUNBUFFERED ansible

--- jobs/scripts/ansible-playbook-generate-vars-file.sh
+++ jobs/scripts/ansible-playbook-generate-vars-file.sh
@@ -5,5 +5,5 @@ user: a
 url: https://github.com/pli01/ansible-samples/archive/master.zip
 dest: ansible-samples-master
 pre_cmd: []
-post_cmd: ["( export PYTHONUNBUFFERED=1 ; cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml -e myvar='' )"]
+post_cmd: ["( export PYTHONUNBUFFERED=$PYTHONUNBUFFERED ; cd ansible-samples-master && ansible-playbook -i hosts -c local site.yml -e myvar='' )"]
 EOF
