b751c4ef757927c9af2934af233048e3eb84a130
pli01 <pli01@github.com>
2016-10-18 23:33:32 +0200

    sample test docker

--- jobs/scripts/ansible-playbook-generate-vars-file.sh
+++ jobs/scripts/ansible-playbook-generate-vars-file.sh
@@ -6,5 +6,5 @@ user: a
 url: https://github.com/pli01/ansible-samples/archive/master.zip
 dest: ansible-samples-master
 pre_cmd: []
-post_cmd: ["( export PYTHONUNBUFFERED=$PYTHONUNBUFFERED ; cd ansible-samples-master && make deploy)"]
+post_cmd: ["( export PYTHONUNBUFFERED=$PYTHONUNBUFFERED ; cd ansible-samples-master && make -C tests )"]
 EOF
